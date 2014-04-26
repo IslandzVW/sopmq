8cf9d01fdfc52fd7a91b73e1574e540f582474f8
David Daeschler <david.daeschler@gmail.com>
2014-04-26 00:12:45 -0400

    Add build directory to .gitignore. Add initial source file.

--- node/src/node-main.cpp
+++ node/src/node-main.cpp
@@ -1,6 +1,7 @@
 /*
+ * SOPMQ - Scalable optionally persistent message queue
  * Copyright 2014 InWorldz, LLC
-
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +13,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
+
+int main(int argc, char* argv[])
+{
+    
+}
 
