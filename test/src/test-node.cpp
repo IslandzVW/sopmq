cff5a1bd98855dc33ce29b3e6bb38357876e2554
David Daeschler <david.daeschler@gmail.com>
2014-04-28 00:56:01 -0400

    Working on unit test for endpoint

--- test/src/test-node.cpp
+++ test/src/test-node.cpp
@@ -22,7 +22,37 @@ TEST(EndPointTest, EndPointParserHandlesNoPort)
     ASSERT_EQ(ep.host_name(), "localhost");
 }
 
+TEST(EndPointTest, EndPointParserHandlesShortHost)
+{
+    endpoint ep("sopmq1://l");
+    
+    ASSERT_EQ(ep.port(), 8481);
+    ASSERT_EQ(ep.protocol(), endpoint::SOPMQv1);
+    ASSERT_EQ(ep.host_name(), "l");
+}
+
 TEST(EndPointTest, EndPointParserThrowsWithEmptyURI)
 {
     ASSERT_THROW(endpoint ep(""), uri_parse_error);
+}
+
+TEST(EndPointTest, EndPointParserThrowsWithNoScheme)
+{
+    ASSERT_THROW(endpoint ep("sopmq"), uri_parse_error);
+}
+
+TEST(EndPointTest, EndPointParserThrowsWithOnlySchemeParts)
+{
+    ASSERT_THROW(endpoint ep("sopmq1:"), uri_parse_error);
+    ASSERT_THROW(endpoint ep("sopmq1:/"), uri_parse_error);
+}
+
+TEST(EndPointTest, EndPointParserThrowsWithOnlyFullScheme)
+{
+    ASSERT_THROW(endpoint ep("sopmq1://"), uri_parse_error);
+}
+
+TEST(EndPointTest, EndPointParserThrowsWithInvalidScheme)
+{
+    ASSERT_THROW(endpoint ep("sopmq5://test"), invalid_protocol_error);
 }
